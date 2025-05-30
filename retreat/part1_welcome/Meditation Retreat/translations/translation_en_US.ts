28542e344b3834834ea55dbe4a1249e156ac0b29
parkkweo <parkkweo@oregonstate.edu>
2025-05-30 15:43:47 -0700

    Added Choregraphe Files

---
+++ retreat/part1_welcome/Meditation Retreat/translations/translation_en_US.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+        <message>
+            <source>Hello, I'm Pepper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, I'm Pepper</translation>
+        </message>
+        <message>
+            <source>Hello, I'm Pepper. I'm recording for the test video!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, I'm Pepper. I'm recording for the test video!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Part 1/Welcome/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, I'm Pepper. I'm so excited to be hosting a meditation retreat with you today!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, I'm Pepper. I'm so excited to be hosting a meditation retreat with you today!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello! I'm Pepper!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello! I'm Pepper!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello, I'm Pepper.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, I'm Pepper.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello! I'm Pepper!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello! I'm Pepper!</translation>
+        </message>
+    </context>
+</TS>
